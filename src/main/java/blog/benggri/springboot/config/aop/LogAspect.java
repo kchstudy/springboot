86edc8c0aad6caf4f1807d9e91d6093aa284ce3c
kch <benggri@gmail.com>
2022-02-11 10:58:48 +0900

    AOP 수정

--- src/main/java/blog/benggri/springboot/config/aop/LogAspect.java
+++ src/main/java/blog/benggri/springboot/config/aop/LogAspect.java
@@ -1,7 +1,9 @@
 package blog.benggri.springboot.config.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,15 @@ public class LogAspect {
 
     @Around("execution(* blog.benggri.springboot..*Controller.*(..))")
     public Object logging(ProceedingJoinPoint pjp) throws Throwable {
-        log.info("start - " + pjp.getSignature().getDeclaringTypeName() + " / " + pjp.getSignature().getName());
+        log.info("[START][" + pjp.getSignature().getDeclaringTypeName() + "][" + pjp.getSignature().getName()+"]");
         Object result = pjp.proceed();
-        log.info("finished - " + pjp.getSignature().getDeclaringTypeName() + " / " + pjp.getSignature().getName());
+        log.info("[FINISHED][" + pjp.getSignature().getDeclaringTypeName() + "][" + pjp.getSignature().getName()+"]");
         return result;
+    }
+
+    @AfterThrowing(pointcut="execution(* blog.benggri.springboot..*Controller.*(..))", throwing="ex")
+    public void loggingAfterThrowing(JoinPoint jp, Throwable ex) throws Throwable {
+        log.info("[ERROR][" + jp.getSignature().getDeclaringTypeName() + "][" + jp.getSignature().getName()+"]");
     }
 
 }
