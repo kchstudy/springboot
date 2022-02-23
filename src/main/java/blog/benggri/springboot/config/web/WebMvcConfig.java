b3a685d7efcf4b53dd724cf7dfe028aa1b18125c
kch <benggri@gmail.com>
2022-02-23 16:17:01 +0900

    mybatis 버전 수정 및 view 설정

---
+++ src/main/java/blog/benggri/springboot/config/web/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package blog.benggri.springboot.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("classpath:/web/resources/")
+                .setCachePeriod(60);
+    }
+
+}
