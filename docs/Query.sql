d0cee3ad0fc908fc9c734728006f7ecfde419104
kch <benggri@gmail.com>
2022-03-04 09:04:21 +0900

    실용 쿼리 수정

--- docs/Query.sql
+++ docs/Query.sql
@@ -63,11 +63,11 @@ and relname = '테이블명'
 /* 쿼리 생성 */
 with tbl_name as (
   select 'usr' as nm /* 테이블명 입력 */
-  , 'select' as type /* 쿼리 타입 */
+  , '' as type /* 쿼리 타입 */
 ),
 tbl_info as (
   select c.relname as tbl_name, obj_description(c.oid) as tbl_desc
-    from pg_catalog.pg_class c inner join pg_catalog.pg_namespace n on c.relnamespace=n.oid
+    from pg_catalog.pg_class c left join pg_catalog.pg_namespace n on c.relnamespace=n.oid
    where c.relkind = 'r'
      and c.relname = (select nm from tbl_name)
 ),
@@ -82,6 +82,10 @@ col_info as (
         , repeat(' ', cast(len.clen as int) - length(c.column_name)) as c_nm_term
         , c.ordinal_position      as c_ord_sq
         , c.udt_name              as dt_type
+        , case when c.udt_name = 'numeric' then concat('cast( #{', c.column_name, '} as ', c.udt_name ,') ')
+                when c.udt_name = 'timestamp' then 'now()'
+                else concat('#{', c.column_name, '}')
+                end as binding_str
         , pgd.description         as c_desc
     from pg_catalog.pg_statio_all_tables st
        , pg_catalog.pg_description       pgd
@@ -103,7 +107,7 @@ sql_template as (
   union all
   select ' where 1=1 ' as query_str, 7000 as order_sq, 'select' as type from tbl_info
   union all
-  select concat('   and ', c_nm, c_nm_term, ' = #{', c_nm, '}', c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (8000 + c_ord_sq) as order_sq, 'select' as type from col_info
+  select concat('   and ', c_nm, c_nm_term, ' = ', binding_str , c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (8000 + c_ord_sq) as order_sq, 'select' as type from col_info
   union all
   select 'update /*  */ ' as query_str, 1000 as order_sq, 'update' as type
   union all
@@ -111,11 +115,11 @@ sql_template as (
   union all
   select '   set ' as query_str, 2000 as order_sq, 'update' as type
   union all
-  select concat('     , ', c_nm, c_nm_term, ' = #{', c_nm, '}', c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (3000 + c_ord_sq) as order_sq, 'update' as type from col_info
+  select concat('     , ', c_nm, c_nm_term, ' = ', binding_str, c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (3000 + c_ord_sq) as order_sq, 'update' as type from col_info
   union all
   select ' where 1=1 ' as query_str, 6000 as order_sq, 'update' as type from tbl_info
   union all
-  select concat('   and ', c_nm, c_nm_term, ' = #{', c_nm, '}', c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (8000 + c_ord_sq) as order_sq, 'update' as type from col_info
+  select concat('   and ', c_nm, c_nm_term, ' = ', binding_str, c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (8000 + c_ord_sq) as order_sq, 'update' as type from col_info
   union all
   select 'insert /*  */ ' as query_str, 1000 as order_sq, 'insert' as type
   union all
@@ -125,7 +129,7 @@ sql_template as (
   union all
   select ') values ( ' as query_str, 6000 as order_sq, 'insert' as type
   union all
-  select concat('     , #{', c_nm, '}', c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (7000 + c_ord_sq) as order_sq, 'insert' as type from col_info
+  select concat('     , ', binding_str, c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (7000 + c_ord_sq) as order_sq, 'insert' as type from col_info
   union all
   select ') ' as query_str, 9000 as order_sq, 'insert' as type
   union all
@@ -135,7 +139,7 @@ sql_template as (
   union all
   select ' where 1=1 ' as query_str, 3000 as order_sq, 'delete' as type
   union all
-  select concat('   and ', c_nm, c_nm_term, ' = #{', c_nm, '}', c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (8000 + c_ord_sq) as order_sq, 'delete' as type from col_info
+  select concat('   and ', c_nm, c_nm_term, ' = ', binding_str, c_nm_term, ' /* ', dt_type, '-', c_desc, ' */' ) as query_str, (8000 + c_ord_sq) as order_sq, 'delete' as type from col_info
 )
 --select * from sql_template
 select query_str from sql_template
