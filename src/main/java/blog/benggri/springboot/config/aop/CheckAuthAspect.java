cbbeedfb6a9952b29f4a48623bfc5135c8ab6c22
kch <benggri@gmail.com>
2022-03-10 17:24:20 +0900

    view 전달을 위한 컨트롤러 AOP token 을 이용한 사용자 ID 조회

--- src/main/java/blog/benggri/springboot/config/aop/CheckAuthAspect.java
+++ src/main/java/blog/benggri/springboot/config/aop/CheckAuthAspect.java
@@ -1,9 +1,17 @@
 package blog.benggri.springboot.config.aop;
 
+import blog.benggri.springboot.comm.exception.CustomException;
+import blog.benggri.springboot.config.jwt.TokenProvider;
+import blog.benggri.springboot.jpa.repository.token.TokenRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +21,15 @@ import static blog.benggri.springboot.comm.util.StringUtil.STEP;
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class CheckAuthAspect {
+
+    @Autowired
+    private final TokenRepository tokenRepository;
+
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final TokenProvider tokenProvider;
 
     @Around("execution(* blog.benggri.springboot.view.ViewController.view(..))")
     public Object logging(ProceedingJoinPoint pjp) throws Throwable {
@@ -28,6 +44,13 @@ public class CheckAuthAspect {
         STEP(log, "token:"+token   );
         STEP(log, "menuId:"+menuId );
         // TODO token 값을 이용한 jwt 검증 및 사용자 id 조회
+        STEP(log, "1. Refresh Token 검증");
+        if (!tokenProvider.validateToken(token)) {
+            throw new CustomException("유효하지 않은 Token");
+        }
+
+        Authentication authentication = tokenProvider.getAuthentication(token);
+        String usrId = authentication.getName();
         // TODO 사용자 id 를 이용한 menuId 권한 검증
         Object result = pjp.proceed(); // 실제 컨트롤러 로직이 수행되는 구간
         return result;
