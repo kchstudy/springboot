108e27981be0fb8548ff2bca18e279659cd4bb3a
kch <benggri@gmail.com>
2022-03-11 17:58:07 +0900

    공통코드 관리 화면 생성

--- src/main/resources/web/resources/js/code/codemng.js
+++ src/main/resources/web/resources/js/code/codemng.js
@@ -6,6 +6,10 @@ $(function(){
              ,grp_nm      : $('#grp_cd_grp_nm').val()
              ,prnt_grp_cd : $('#grp_cd_prnt_grp_cd').val()
              ,use_yn      : $('#grp_cd_use_yn').val()
+             ,page        : {
+                  page_no      : '1'
+                 ,cnt_per_page : $('#grp_cd_page_cnt').val()
+             }
          }
          ,cm_util.STEP
          ,cm_util.STEP
