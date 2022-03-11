276cd6d7a5cf3ff0976d409d7001ebb238df9574
kch <benggri@gmail.com>
2022-03-11 15:03:20 +0900

    view 전달 추가

--- src/main/java/blog/benggri/springboot/config/aop/CheckAuthAspect.java
+++ src/main/java/blog/benggri/springboot/config/aop/CheckAuthAspect.java
@@ -1,22 +1,15 @@
 package blog.benggri.springboot.config.aop;
 
-import blog.benggri.springboot.comm.exception.CustomException;
 import blog.benggri.springboot.config.jwt.TokenProvider;
-import blog.benggri.springboot.jpa.repository.token.TokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-
-import static blog.benggri.springboot.comm.util.StringUtil.STEP;
 
 @Slf4j
 @Aspect
@@ -24,11 +17,6 @@ import static blog.benggri.springboot.comm.util.StringUtil.STEP;
 @RequiredArgsConstructor
 public class CheckAuthAspect {
 
-    @Autowired
-    private final TokenRepository tokenRepository;
-
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final TokenProvider tokenProvider;
 
     @Around("execution(* blog.benggri.springboot.view.ViewController.view(..))")
